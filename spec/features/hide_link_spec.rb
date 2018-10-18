ab5b2f5b8cb0c330912af622c015e492840ae878
Nate Miller <natekmiller96@gmail.com>
2018-10-18 21:42:56 +0000

    automatically backed up by learn

--- spec/features/hide_link_spec.rb
+++ spec/features/hide_link_spec.rb
@@ -4,6 +4,7 @@ feature "Hide Link", :js => true do
   scenario "Clicking on 'Click To Hide Me' hides itself" do
     visit '/'
     click_link 'Click To Hide Me'
+    byebug
     expect(page).not_to have_content 'Click To Hide Me'
   end
 end
